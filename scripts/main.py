42d479b23b4b98475f8aa46844fae40f87c48bf7
Davide Ghiotto <ghiotto.davidenko@gmail.com>
2021-03-01 17:05:43 +0100

    basic solution implemented

--- scripts/main.py
+++ scripts/main.py
@@ -10,7 +10,7 @@ intersections = {}
 # * main function
 
 
-def solve(lines):
+def solve(name, lines):
     # ? parse start
     D, I, S, V, F = map(lambda x: int(x), lines[0].split(" "))
     # print(D, I, S, V, F)
@@ -21,10 +21,14 @@ def solve(lines):
         B, E, street_name, L = l.split(" ")
         street_name = street_name.replace("\n", "")
         streets[street_name] = {
-            'i_start': int(B),
-            'i_end': int(E),
+            'i_start': B,
+            'i_end': E,
             'length': int(L)
         }
+        if intersections.get(E) is None:
+            intersections[E] = [street_name]
+        else:
+            intersections[E].append(street_name)
         # print(streets[street_name])
     # print("--- paths ---")
     lines_paths = lines[S:S+V]
@@ -36,14 +40,30 @@ def solve(lines):
         # print(streets_names)
     # ? parse end
 
+    # ? algorithm
+
+    # ? output
+    with open(f"../submissions/{name}.txt", "w") as f:
+        f.write(f"{len(intersections)} \n")  # intersections
+        for intersection in intersections:
+            f.write(intersection+"\n")
+            intersection_streets = intersections.get(intersection)
+            f.write(f"{len(intersection_streets)} \n")
+            for street in intersection_streets:
+                time = 1
+                f.write(f"{street} {time}\n")
+
 
 # files_name = files_name[:1]
 bar = ChargingBar("Solving", max=len(files_name))
 # * for every input test file, solve
 for name in files_name:
+    cars = []
+    streets = {}
+    intersections = {}
     f = open(f"../inputs/{name}.txt", "r")
     lines = f.readlines()
     f.close()
-    solve(lines)
+    solve(name, lines)
     bar.next()
 bar.finish()
